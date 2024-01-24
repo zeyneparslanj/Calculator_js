6356fb4bca3b9bb03a572030ab67db73796eba3d
zeyneparslanj <zynp.arsln274@gmail.com>
2024-01-24 16:35:31 +0300

    l

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 function Num(val){
     document.getElementById('result').value += val;
+    
 }
 
 function equal(){
@@ -11,3 +12,28 @@ function equal(){
 function clr(){
     document.getElementById('result').value = '';
 }
+
+// Klavye olayları için dinleyici ekle
+document.addEventListener('keydown', function(event) {
+    const key = event.key;
+    const validKeys = /[0-9\+\-\*\/\=,C,]/;
+
+    if (validKeys.test(key)) {
+        event.preventDefault(); // Tuşun varsayılan davranışını engelle
+        if (key === 'C') {
+            clr();
+        } else if (key === '=') {
+            equal();
+        } else {
+            Num(key);
+        }
+    }
+});
+
+// Enter tuşunu kontrol et
+document.getElementById('result').addEventListener('keydown', function(event) {
+    // if (event.key === 'Enter') {
+    //     event.preventDefault(); // Enter'ın varsayılan davranışını engelle
+    //     equal();
+    // }
+});
